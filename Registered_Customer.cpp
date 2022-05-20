ef351d2d7f27930528a8c67c4aa2245f7a7ea135
IT21272554 <100111673+IT21272554@users.noreply.github.com>
2022-05-20 23:44:27 +0530

    Create Registered_Customer.cpp

---
+++ Registered_Customer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include "Registered_Customer.h"
+using namespace std;
+Registered_Customer::Registered_Customer()
+{
+	user_name = " ";
+	password = " ";
+}
+
+Registered_Customer::Registered_Customer(string puser_name, string ppassword)
+{
+
+}
+
+void Registered_Customer::login()
+{
+
+}
+
+void Registered_Customer::displaydetails()
+{
+
+}
+
+void Registered_Customer::set_details(string pusername, string ppassword)
+{
+
+}
+
+void Registered_Customer::logout()
+{
+
+}
+
+Registered_Customer::~Registered_Customer()
+{
+
+}
