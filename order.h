2ccc3366b5356d425f584ecf3e4d9ac718396850
Tharushi Kodithuwakku <99242653+it21269202@users.noreply.github.com>
2022-05-20 23:45:18 +0530

    Create order.h

---
+++ order.h
@@ -0,0 +1,19 @@
+//IT21269202 Kodithuwakku T.N.
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Order
+{
+private:
+	int orderno;
+	string orderid;
+Registered_Customer* cus;
+
+public:
+	Order();
+	Order(int porderno, string porderid);
+	void addOrder(Payment* Pay, Reports* Rep);
+	void displayorderlist();
+	~Order();
+};
