054091e5330dc1e5af4f4eacaca16f652a495dfa
Tharushi Kodithuwakku <99242653+it21269202@users.noreply.github.com>
2022-05-20 23:43:00 +0530

    Create payment.cpp

---
+++ payment.cpp
@@ -0,0 +1,41 @@
+//IT21269202 Kodithuwakku T.N.
+#include <iostream>
+#include <cstring>
+#include "Payment.h"
+using namespace std;
+Payment::Payment()
+{
+	payment_type = " ";
+	payment_ID = " ";
+	amount = " ";
+}
+
+Payment::Payment(string ppayment_type, string ppayment_ID, double pamount)
+{
+
+}
+
+void Payment::display_payment_methods()
+{
+
+}
+
+double Payment::calculate_amount()
+{
+
+}
+
+double Payment::display_amount()
+{
+
+}
+
+void Payment::store_payment_details()
+{
+
+}
+
+Payment::~Payment()
+{
+
+}
