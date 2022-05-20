725f123a9c64418dbda553246131058d1f92c2b7
NavodDeshan <99177531+NavodDeshan@users.noreply.github.com>
2022-05-20 23:36:14 +0530

    Create service.cpp

---
+++ service.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include "Service.h"
+using namespace std;
+Service::Service()
+{
+	servicetype = " ";
+	servicecharge = " ";
+	weight = " ";
+}
+
+Service::Service(string pservicetype, double pservicecharge, float pweight)
+{
+
+}
+
+void Service::addCustomer()
+{
+
+}
+
+void Service::displayservicetype()
+{
+
+}
+
+void Service::displayservicecharge()
+{
+
+}
+
+void Service::displayweight()
+{
+
+}
+
+Service::~Service()
+{
+
+}
